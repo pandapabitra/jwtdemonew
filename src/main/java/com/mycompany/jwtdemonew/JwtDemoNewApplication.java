569de48991a1730fed9cad3eeb1a1cede727da50
Pabitra Panda <pk.pabitra18@gmail.com>
2022-08-26 00:38:44 +0530

    initial commit

---
+++ src/main/java/com/mycompany/jwtdemonew/JwtDemoNewApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.jwtdemonew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDemoNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtDemoNewApplication.class, args);
+	}
+
+}
